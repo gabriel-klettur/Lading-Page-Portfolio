f4d54425b5c58939d85ee3d94c2a6e8012f555ad
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-22 10:46:27 +0000

    style(Font): Implemented 'Roboto' from google api fonts

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Roboto será la fuente principal
+      },
+    },
   },
   plugins: [],
 }
